58400d30da0ac23c7a2c124cb5080aef1de19c2b
Dawid Janiga <dawidjaniga@gmail.com>
2019-06-22 13:46:08 +0200

    build(package): Build package in UMD format

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -1,20 +1,17 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
 import pkg from './package.json'
 
 export default {
   input: 'src/map-countdown.js',
   output: [
     {
-      file: 'examples/bundle.js',
+      file: 'dist/bundle.js',
       format: 'umd',
       name: 'MapCountdown'
     }
   ],
-  sourcemap: true,
   external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
     resolve(),
@@ -23,11 +20,6 @@ export default {
       presets: [['@babel/env', { modules: false }]],
       exclude: 'node_modules/**'
     }),
-    postcss(),
-    livereload('examples'),
-    serve({
-      open: true,
-      contentBase: './examples/'
-    })
+    postcss()
   ]
 }
